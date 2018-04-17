7d4419d12cf5dc721c2f1adcd124cd1d5056c060
Jian Li <pyguni@gmail.com>
2018-04-18 00:11:40 +0900

    Add sequence number for two ONOS source directory
    This commit allows the BUCK test build tool always refer to the
    stable-onos repo first.

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-MASTER_ONOS=master-onos
+MASTER_ONOS=02-master-onos
 MASTER_ONOS_APPS=$MASTER_ONOS/apps
 LOCAL_APPS=apps
 BUCK_OUT=buck-out
 SONA_OUT=sona-out
+BUCK=bin/buck
 
 rm -rf $LOCAL_APPS
 mkdir -p $LOCAL_APPS
@@ -14,18 +15,15 @@ cp -R $MASTER_ONOS_APPS/openstacknetworking $LOCAL_APPS
 cp -R $MASTER_ONOS_APPS/openstacknode $LOCAL_APPS
 cp -R $MASTER_ONOS_APPS/openstacknetworkingui $LOCAL_APPS
 
-# remove master-onos
-rm -rf $MASTER_ONOS
-
 # start to build sona and corresponding artifacts
-bin/buck build onos
+$BUCK build onos
 
 # copy SONA artifacts into the sona-out directory
-# rm -rf $SONA_OUT
-# mkdir -p $SONA_OUT
-# cp $BUCK_OUT/gen/apps/openstacknetworking/onos-apps-openstacknetworking-oar/app.oar $SONA_OUT/openstacknetworking.oar
-# cp $BUCK_OUT/gen/apps/openstacknode/onos-apps-openstacknode-oar/app.oar $SONA_OUT/openstacknode.oar
-# cp $BUCK_OUT/gen/apps/openstacknetworkingui/onos-apps-openstacknetworkingui-oar/app.oar $SONA_OUT/openstacknetworkingui.oar
+rm -rf $SONA_OUT
+mkdir -p $SONA_OUT
+cp $BUCK_OUT/gen/apps/openstacknetworking/onos-apps-openstacknetworking-oar/app.oar $SONA_OUT/openstacknetworking.oar
+cp $BUCK_OUT/gen/apps/openstacknode/onos-apps-openstacknode-oar/app.oar $SONA_OUT/openstacknode.oar
+cp $BUCK_OUT/gen/apps/openstacknetworkingui/onos-apps-openstacknetworkingui-oar/app.oar $SONA_OUT/openstacknetworkingui.oar
 
 # clean
 # rm -rf $LOCAL_APPS
--- verify.sh
+++ verify.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+BUCK=bin/buck
 
 # run unit test for openstacknetworking
-bin/buck test //apps/openstacknetworking/app:onos-apps-openstacknetworking-app-tests
+$BUCK test //apps/openstacknetworking/app:onos-apps-openstacknetworking-app-tests
 
 # run unit test for openstacknode
-bin/buck test //apps/openstacknode/app:onos-apps-openstacknode-app-tests
+$BUCK test //apps/openstacknode/app:onos-apps-openstacknode-app-tests
