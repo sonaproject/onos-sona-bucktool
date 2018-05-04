e4273a74f0bdec8eaac234af79c33072912df7c4
Jian Li <pyguni@gmail.com>
2018-05-04 13:58:15 +0900

    Add git meta information before patch ONOS

--- patch.sh
+++ patch.sh
@@ -12,6 +12,9 @@ fi
 cp $ONOS_PATCH/$1/*.patch $STABLE_ONOS
 cd $STABLE_ONOS
 
+git config --global user.email "sona@onosproject.com"
+git config --global user.name "SONA Project"
+
 for entry in *.patch
 do
   echo "Patching ONOS with the patch $entry..."
