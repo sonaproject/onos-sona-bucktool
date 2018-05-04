45f7db64fdd9440663b25e2735f27286923dc784
Jian Li <pyguni@gmail.com>
2018-05-04 13:41:52 +0900

    Add a shell script to patch stable ONOS source

---
+++ patch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+STABLE_ONOS=01-stable-onos
+ONOS_PATCH=03-onos-patch
+
+if [ -z "$1" ]
+  then
+    echo "Error: need to specify ONOS version numnber!"
+    exit 1
+fi
+
+cp $ONOS_PATCH/$1/*.patch $STABLE_ONOS
+cd $STABLE_ONOS
+
+for entry in *.patch
+do
+  echo "Patching ONOS with the patch $entry..."
+  git am -3 < $entry
+done
+
+echo "Done all patches!"
