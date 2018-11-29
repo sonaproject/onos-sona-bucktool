cb0d86737ae94cdd299c51ae306506b56494cc22
Jian Li <pyguni@gmail.com>
2018-11-29 10:48:24 +0900

    Copy BUCK build script from stable to staging directory

--- build.sh
+++ build.sh
@@ -25,6 +25,31 @@ cp -R $MASTER_ONOS_APPS/openstacktelemetry $LOCAL_APPS
 cp -R $MASTER_ONOS_APPS/openstackvtap $LOCAL_APPS
 cp -R $MASTER_ONOS_APPS/openstacktroubleshoot $LOCAL_APPS
 
+# copy BUCK build script
+cp $STABLE_ONOS_APPS/openstacknetworking/BUCK $LOCAL_APPS/openstacknetworking
+cp $STABLE_ONOS_APPS/openstacknetworking/api/BUCK $LOCAL_APPS/openstacknetworking/api
+cp $STABLE_ONOS_APPS/openstacknetworking/app/BUCK $LOCAL_APPS/openstacknetworking/app
+
+cp $STABLE_ONOS_APPS/openstacknode/BUCK $LOCAL_APPS/openstacknode
+cp $STABLE_ONOS_APPS/openstacknode/api/BUCK $LOCAL_APPS/openstacknode/api
+cp $STABLE_ONOS_APPS/openstacknode/app/BUCK $LOCAL_APPS/openstacknode/app
+
+cp $STABLE_ONOS_APPS/openstacknetworkingui/BUCK $LOCAL_APPS/openstacknetworkingui
+cp $STABLE_ONOS_APPS/openstacknetworkingui/api/BUCK $LOCAL_APPS/openstacknetworkingui/api
+cp $STABLE_ONOS_APPS/openstacknetworkingui/app/BUCK $LOCAL_APPS/openstacknetworkingui/app
+
+cp $STABLE_ONOS_APPS/openstacktelemetry/BUCK $LOCAL_APPS/openstacktelemetry
+cp $STABLE_ONOS_APPS/openstacktelemetry/api/BUCK $LOCAL_APPS/openstacktelemetry/api
+cp $STABLE_ONOS_APPS/openstacktelemetry/app/BUCK $LOCAL_APPS/openstacktelemetry/app
+
+cp $STABLE_ONOS_APPS/openstackvtap/BUCK $LOCAL_APPS/openstackvtap
+cp $STABLE_ONOS_APPS/openstackvtap/api/BUCK $LOCAL_APPS/openstackvtap/api
+cp $STABLE_ONOS_APPS/openstackvtap/app/BUCK $LOCAL_APPS/openstackvtap/app
+
+cp $STABLE_ONOS_APPS/openstacktroubleshoot/BUCK $LOCAL_APPS/openstacktroubleshoot
+cp $STABLE_ONOS_APPS/openstacktroubleshoot/api/BUCK $LOCAL_APPS/openstacktroubleshoot/api
+cp $STABLE_ONOS_APPS/openstacktroubleshoot/app/BUCK $LOCAL_APPS/openstacktroubleshoot/app
+
 # replace broken deps
 sed -i 's/com_google_code_gson_gson/gson/g' $LOCAL_APPS/openstacktelemetry/BUCK
 
